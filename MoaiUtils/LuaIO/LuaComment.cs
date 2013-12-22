5ba2400f36f3e2805f7cdcee4b053724a42afe04
Daniel Wolf <dwolf@dannad.de>
2013-12-22 11:19:05 +0100

    Added convenience constructor for LuaComment taking a list of lines

--- MoaiUtils/LuaIO/LuaComment.cs
+++ MoaiUtils/LuaIO/LuaComment.cs
@@ -1,10 +1,17 @@
-﻿namespace MoaiUtils.LuaIO {
+﻿using System;
+using System.Collections.Generic;
+using MoaiUtils.Tools;
+
+namespace MoaiUtils.LuaIO {
     public class LuaComment {
         public LuaComment(string text, bool blankLineBefore = false, bool blankLineAfter = false) {
             Text = text;
             BlankLineBefore = blankLineBefore;
             BlankLineAfter = blankLineAfter;
         }
+
+        public LuaComment(IEnumerable<string> lines, bool blankLineBefore = false, bool blankLineAfter = false)
+            : this(lines.Join(Environment.NewLine), blankLineBefore, blankLineAfter) {}
 
         public string Text { get; private set; }
         public bool BlankLineBefore { get; private set; }
