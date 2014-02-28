572f4355f742966a5f66ace7f25c840d0fce70bb
Daniel Wolf <dwolf@dannad.de>
2014-02-28 22:26:25 +0100

    Version 2.0

--- MoaiUtils/Common/Properties/SharedAssemblyInfo.cs
+++ MoaiUtils/Common/Properties/SharedAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
